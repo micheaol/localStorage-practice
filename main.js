4c4146a7cd6c15caedbce321066c9783bbec52ad
micheaol <micheaol80@gmail.com>
2021-10-08 16:16:28 +0100

    feat: add logic to remove data

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
-const emailinput = document.querySelector('#email-id');
-const form = document.querySelector('form');
-const displayOut = document.querySelector('.text')
-const storedItem = localStorage.getItem('userEmail');
+const saveTolocalForm = document.querySelector('#add-to-local');
+const removeFromLocalForm = document.querySelector('#remove-local');
+const emailId = document.querySelector('#email-id');
+const addKey = document.querySelector('#key');
+const removeKey = document.querySelector('#removeKey');
 
-if(storedItem){
-    emailinput.value = storedItem
+
+saveTolocalForm.addEventListener('submit', createNewEmail);
+removeFromLocalForm.addEventListener('submit', removeLocal)
+const emailData = [];
+
+function EmailData(key, email){
+    this.key = key;
+    this.email = email;
 }
 
-
-emailinput.addEventListener('input', (e) =>{
-    e.preventDefault();
-    console.log(e.target.value)
-});
-
-const saveToLocalStorage = ()=>{
-    localStorage.setItem('userEmail', emailinput.value);
+function createNewEmail() {
+    const newEmailData = new EmailData(addKey.value, emailId.value)
+    emailData.push(newEmailData);
+    saveToLocal()
 }
 
-form.addEventListener('submit', saveToLocalStorage);
+function saveToLocal(){
+    let key = addKey.value;
+    localStorage.setItem(key, JSON.stringify(emailData))
+}
+
+function removeLocal(){
+    let key = removeKey.value;
+    localStorage.removeItem(key)
+}
